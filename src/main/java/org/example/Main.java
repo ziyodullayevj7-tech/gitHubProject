ca2cf46545f9f3aad266baa70b047608e78f7ab7
jahongir <ziyodullayevj7@gmail.com>
2026-02-23 13:21:41 +0500

    code qushildi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,9 @@ package org.example;
 public class Main {
     static void main() {
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            System.out.println(i);
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
         }
     }
 }
