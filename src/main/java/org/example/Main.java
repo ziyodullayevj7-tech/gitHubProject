be0502f04274eeb8a34c578dfc8deb5f610c2e9f
jahongir <ziyodullayevj7@gmail.com>
2026-02-23 13:08:33 +0500

    1chi commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            System.out.println(i);
+        }
+    }
+}
